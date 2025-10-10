bee1543b7325918dcaa7e8ad6e22122f52e7a410
aspectzxcc <josh10nathan@gmail.com>
2025-10-11 01:18:09 +0800

    added working config

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
-class Config {};
+#include <string>
+
+class Config {
+public:
+  Config();
+  void load(const std::string &filename);
+  int get_num_cpu() { return num_cpu; }
+  std::string get_scheduler() { return scheduler; }
+  int get_quantum_cycles() { return quantum_cycles; }
+  int get_batch_process_freq() { return batch_process_freq; }
+  int get_min_ins() { return min_ins; }
+  int get_max_ins() { return max_ins; }
+  int get_delay_per_exec() { return delay_per_exec; }
+
+private:
+  int num_cpu;
+  std::string scheduler;
+  int quantum_cycles;
+  int batch_process_freq;
+  int min_ins;
+  int max_ins;
+  int delay_per_exec;
+};
---
+++ src/Config.cpp
@@ -0,0 +1,43 @@
+#include "Config.hpp"
+#include <fstream>
+#include <iostream>
+
+Config::Config() {
+  num_cpu = 4;
+  scheduler = "rr";
+  quantum_cycles = 5;
+  batch_process_freq = 1;
+  min_ins = 1000;
+  max_ins = 2000;
+  delay_per_exec = 0;
+}
+
+void Config::load(const std::string &filename) {
+  std::ifstream config_file(filename);
+  if (!config_file.is_open()) {
+    throw std::runtime_error("Could not open config file: " + filename);
+  }
+
+  std::string key, val;
+  while (config_file >> key >> val) {
+    if (key == "num-cpu") {
+      num_cpu = std::stoi(val);
+    } else if (key == "scheduler") {
+      scheduler = val;
+    } else if (key == "quantum-cycles") {
+      quantum_cycles = std::stoi(val);
+    } else if (key == "batch-process-freq") {
+      batch_process_freq = std::stoi(val);
+    } else if (key == "min-ins") {
+      min_ins = std::stoi(val);
+    } else if (key == "max-ins") {
+      max_ins = std::stoi(val);
+    } else if (key == "delay-per-exec") {
+      delay_per_exec = std::stoi(val);
+    } else {
+      std::cerr << "Unknown config key: " << key << std::endl;
+    }
+  }
+
+  config_file.close();
+}
