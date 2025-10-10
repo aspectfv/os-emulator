7eb728eb462379eda78957cf400862981d66c406
aspectzxcc <josh10nathan@gmail.com>
2025-10-11 02:09:35 +0800

    turned config getters into const methods

--- include/Config.hpp
+++ include/Config.hpp
@@ -9,13 +9,13 @@ public:
     return instance;
   }
   void load(const std::string &filename);
-  int get_num_cpu() { return num_cpu; }
-  std::string get_scheduler() { return scheduler; }
-  int get_quantum_cycles() { return quantum_cycles; }
-  int get_batch_process_freq() { return batch_process_freq; }
-  int get_min_ins() { return min_ins; }
-  int get_max_ins() { return max_ins; }
-  int get_delay_per_exec() { return delay_per_exec; }
+  int get_num_cpu() const { return num_cpu; }
+  std::string get_scheduler() const { return scheduler; }
+  int get_quantum_cycles() const { return quantum_cycles; }
+  int get_batch_process_freq() const { return batch_process_freq; }
+  int get_min_ins() const { return min_ins; }
+  int get_max_ins() const { return max_ins; }
+  int get_delay_per_exec() const { return delay_per_exec; }
 
 private:
   Config();
