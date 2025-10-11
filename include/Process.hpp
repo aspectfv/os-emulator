96fb5e52b36033ecb841e587b8780e2f3f057fa2
aspectzxcc <josh10nathan@gmail.com>
2025-10-11 22:06:40 +0800

    added cpu core class and changed instructions vector to unique ptrs for polymorphism

--- include/Process.hpp
+++ include/Process.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "instructions/IInstruction.hpp"
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -9,7 +10,7 @@ private:
   int id_;
   std::string name_;
   std::string created_at_;
-  std::vector<IInstruction> instructions_;
+  std::vector<std::unique_ptr<IInstruction>> instructions_;
   int total_instructions_;
   int instruction_pointer_;
   enum class State { NEW, READY, RUNNING, WAITING, TERMINATED } state_;
---
+++ src/CPUCore.cpp
@@ -0,0 +1,1 @@
+#include "CPUCore.hpp"
