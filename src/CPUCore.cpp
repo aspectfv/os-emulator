1c04cc621e506bfd27d10cedd485635cc2f0f31c
aspectzxcc <josh10nathan@gmail.com>
2025-10-15 04:29:04 +0800

    removed unused import

--- src/CPUCore.cpp
+++ src/CPUCore.cpp
@@ -1,7 +1,6 @@
 #include "CPUCore.hpp"
 #include "Process.hpp"
 #include <chrono>
-#include <iostream>
 
 CPUCore::CPUCore(int id, const std::string scheduler, const int delay_per_exec)
     : id_(id), scheduler_(scheduler), delay_per_exec_(delay_per_exec) {}
